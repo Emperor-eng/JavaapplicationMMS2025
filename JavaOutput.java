7f5ada7e47a68554c4c86137ecb31d4faafdf365
Emperor-eng <misterquality180@gmail.com>
2026-02-18 05:06:29 -0600

    UserInput

--- JavaOutput.java
+++ JavaOutput.java
@@ -4,7 +4,15 @@ public static void main(String[] args){
 	System.out.println("Hello world, this is my first Java Program");
 	}	
 	
+	//Using the println method
+	System.out.println(
 	
+	//Using the print method
+	System.out.print("Do you love learning Java?");
+	System.out.print("Yes, i love learning Java ");
+	
+	//Using the printf method
+	System.out.printf("My name is Egbe Nnamdi, i am %d years old %n",25);
 	
 	
 	
---
+++ new 1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UserInput{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.print("Please enter your full name: ");
+		String fullName = scan.nextLine();
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+
+
